4cb8661a4eb3bef69163841541145b75a58e17ee
Garv2003 <aggarwalgarv0505@gmail.com>
2024-12-22 08:03:07 +0530

    add dockerfile

--- isitChristmas/urls.py
+++ isitChristmas/urls.py
@@ -2,9 +2,10 @@
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, path
 
 urlpatterns = [
-    path("", include("hello.urls")),
-    path("static/", include("django.contrib.staticfiles.urls")),
+    path("", include("hello.urls")),  
 ]
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
